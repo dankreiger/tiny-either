aa2b1d00c2c09f2815e02e59040d7df8233402bc
Dan Kreiger <dan@dankreiger.com>
2021-10-01 20:34:41 +0200

    fix(esm): remove commonjs bundle

--- src/Either/dev/types/T_InspectVal.types.ts
+++ src/Either/dev/types/T_InspectVal.types.ts
@@ -1,2 +1,5 @@
-/** @since 1.0.6 */
+/**
+ * @since 1.0.6
+ * @internal
+ */
 export type T_InspectVal<T extends 'Left' | 'Right'> = `${T}(${string})`;
--- src/Either/index.ts
+++ src/Either/index.ts
@@ -1,4 +1,3 @@
-import { inspect } from './dev';
 import type { I_Either } from './interfaces';
 import { Left } from './Left';
 import { Right } from './Right';
@@ -7,12 +6,9 @@ import { fromNullable, tryCatch } from './transformers';
 /**
  * @since 1.0.0
  */
-const Either: I_Either = {
+export const Either: I_Either = {
   fromNullable,
   tryCatch,
   Left,
   Right,
 };
-
-export { inspect };
-export default Either;
